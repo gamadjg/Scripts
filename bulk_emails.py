c42355a19988c586b4e5b144ff8412fbd94a7a35
David Gama <gamadavid36@gmail.com>
2020-04-09 22:02:32 -0700

    Reading email list, Creating email, and opening it complete

--- bulk_emails.py
+++ bulk_emails.py
@@ -1,12 +1,14 @@
 # To send out bulk emails to all staff
-
 # Read emails from a csv list
-
 # Create body of email & add attachments if necessary
-
 # Can we open outlook and auto-insert all emails from the list as people to send to?
 import win32com.client as win32
 import os
+from tkinter import Tk
+from tkinter.filedialog import askopenfilename
+import csv
+import re
+import pandas as pd
 
 def Emailer(text, subject, recipient):
     outlook = win32.Dispatch('outlook.application')
@@ -15,10 +17,41 @@ def Emailer(text, subject, recipient):
     mail.Subject = subject
     mail.HtmlBody = text
     mail.Display(True)
-    print('success')
-    os._exit()
 
+def get_list_path():
+    # Open file browser GUI to select email list
+    Tk().withdraw() # Prevent full GUI from appearing
+    filepath = askopenfilename() # Return selected file path
+    return filepath
+
+def import_list(filepath):
+    list = ''
+    newlist=''
+    #df = pd.read_csv(filepath)
+    #email_col = df['Emails']
+    #return email_col.values.tolist()
+
+    with open(filepath, encoding='utf-8-sig') as csvfile:
+        list = csvfile.readlines()
+        for entry in list:
+            newln = re.sub(r"\n",'; ', entry)
+            newlist +=newln
+    return newlist
+
+#-------------------Working-------------------------------------
+filepath = get_list_path()
+recipients = import_list(filepath)
 subject = input("Enter subject: ")
 body = input("Enter base body: ")
-recipients = 'gamadavid36@gmail.com'
 Emailer(body, subject, recipients)
+
+#------------------Test fields---------------------------------
+#subject = 'Test subject'
+#body = "Test body"
+#recipients = 'gamadavid36@gmail.com'
+#--------------------------------------------------------------
+#filepath = 'C:/Users/djgama/Sync/zoomus_users_test.csv'
+#user_list = import_list(filepath)
+#print(user_list)
+#Emailer(body, subject, user_list)
+#Emailer(body, subject, 'gamadavid@gmail.com; david@clarkeconsulting.com')
