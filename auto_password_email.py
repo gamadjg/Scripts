f2fb291352efb96e728cbcb4ed2955c5d2d96bbc
David Gama <gamadavid36@gmail.com>
2020-04-10 00:05:32 -0700

    successfully pulls up latest password expiration email

--- auto_password_email.py
+++ auto_password_email.py
@@ -1,26 +1,26 @@
 import win32com.client
 import datetime as dt
-
 outlook = win32com.client.Dispatch("Outlook.Application").GetNamespace("MAPI")
-inbox = outlook.GetDefaultFolder(6) # "6" refers to the index of a folder - in this case,
-                                    # the inbox. You can change that number to reference
-                                    # any other folder
+#if outlook.GetDefaultFolder(6) == "Inbox":
+inbox = outlook.GetDefaultFolder(6)
 
-password_exp_folder = ""
+def find_password_exp_folder(inbox):
+    for i in range(inbox.Folders.Count):
+        # Print the name of each folder within default email account/inbox
+        #print(inbox.Folders[i].name)
 
-#print(inbox.Folders.Count)
-#print(inbox.Folders[5].name)
+        # Find the password expiration folder
+        if inbox.Folders[i].name == 'Password Expirations':
+            password_exp_folder = i
+    return password_exp_folder
 
-for i in range(inbox.Folders.Count):
-    # Get the return a list of all the folders within the main email account/inbox
-    #print(inbox.Folders[i].name)
+#def find_latest_email():
 
-    if inbox.Folders[i].name == 'Password Expirations':
-        password_exp_folder = i
-
-print(inbox.Folders[password_exp_folder].Items)
-
-
+pwd_folder = find_password_exp_folder(inbox)
+#print(inbox.Folders[pwd_folder].Items)
+messages = inbox.Folders[pwd_folder].Items
+last_email = messages.GetLast()
+print(last_email.body)
 #messages = inbox.Items
 #message = messages.GetLast()
 #body_content = message.body
